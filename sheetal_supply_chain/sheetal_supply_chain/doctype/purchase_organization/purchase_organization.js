ce7d333d5f5c0097781de35c7e27a6688fcf257f
Mansi <mansi@sanskartechnolab.com>
2025-11-17 18:07:06 +0530

    First commit

---
+++ sheetal_supply_chain/sheetal_supply_chain/doctype/purchase_organization/purchase_organization.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Sanskar Technolab Pvt Ltd and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Purchase Organization", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ sheetal_supply_chain/sheetal_supply_chain/doctype/purchase_organization/test_purchase_organization.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Sanskar Technolab Pvt Ltd and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestPurchaseOrganization(FrappeTestCase):
+	pass
